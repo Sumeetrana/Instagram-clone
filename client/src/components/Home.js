caecef0ebd0f912048ef62cab8705f4dc19de3f2
Sumeetrana <sumeetsinh28@gmail.com>
2020-10-20 02:08:51 +0530

    Home page UI

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,77 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>ramesh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <div>
+            <span>
+              <strong>User</strong>
+            </span>
+            <span>Amazing scenary</span>
+          </div>
+          <input type="text" placeholder="Leave comment.." />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>ramesh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+
+          <div>
+            <span>
+              <strong>User</strong>
+            </span>
+            <span>Amazing scenary</span>
+          </div>
+          <input type="text" placeholder="Leave comment.." />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>ramesh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <div>
+            <span>
+              <strong>User</strong>
+            </span>
+            <span>Amazing scenary</span>
+          </div>
+          <input type="text" placeholder="Leave comment.." />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>ramesh</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <div>
+            <span>
+              <strong>User</strong>
+            </span>
+            <span>Amazing scenary</span>
+          </div>
+          <input type="text" placeholder="Leave comment.." />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
