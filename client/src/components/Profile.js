63b43fa154c1d7ba9268e4824c9265a2d22a8b0b
Sumeetrana <sumeetsinh28@gmail.com>
2020-10-20 01:56:44 +0530

    Profile page UI completed

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,96 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div style={{ maxWidth: "950px", margin: "10px auto" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          margin: "18px 0",
+        }}
+      >
+        <div>
+          <img
+            style={{
+              width: "160px",
+              height: "160px",
+              borderRadius: "80px",
+              objectFit: "cover",
+            }}
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+          />
+        </div>
+        <div style={{ textAlign: "left" }}>
+          <h4>Katy Perry</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              width: "120%",
+            }}
+          >
+            <h6>
+              <b>40</b> posts
+            </h6>
+            <h6>
+              <b>40</b> followers
+            </h6>
+            <h6>
+              <b>40</b> following
+            </h6>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="gallery">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
