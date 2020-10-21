ca95c06b6abf586892e7a87adc3016e15cde7d96
Sumeetrana <sumeetsinh28@gmail.com>
2020-10-21 10:35:52 +0530

    Like unlike api created

--- routes/post.js
+++ routes/post.js
@@ -43,4 +43,31 @@ router.get("/myposts", requireLogin, async (req, res) => {
     console.log("Error: ", e);
   }
 });
+
+router.put("/like", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $push: { likes: req.user._id } },
+    { new: true }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    }
+    res.json(result);
+  });
+});
+
+router.put("/unlike", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    }
+    res.json(result);
+  });
+});
+
 module.exports = router;
