54a2b478197ea6485289bf4ebb2a34939b8d5774
Sumeetrana <sumeetsinh28@gmail.com>
2020-10-21 12:09:59 +0530

    Comment api created

--- models/post.js
+++ models/post.js
@@ -14,6 +14,12 @@ const postSchema = new mongoose.Schema({
     required: true,
   },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [
+    {
+      text: String,
+      postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    },
+  ],
   postedBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
